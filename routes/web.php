06c0cb1f28065bbc1a568c9f6458bb814aa824d6
Ruzbihan <ruzbihanmzm@gmail.com>
2025-06-04 15:47:40 +0530

    define routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/tasks/create', [TaskController::class, 'create']);
+Route::post('/tasks', [TaskController::class], 'store');
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
