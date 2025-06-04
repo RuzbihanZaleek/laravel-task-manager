082e4741640c6dc7cc1e62b5da70262b19148435
Ruzbihan <ruzbihanmzm@gmail.com>
2025-06-04 15:44:48 +0530

    create controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //show all tasks
+    public function index() {
+        $tasks = Task::all();
+        return view('tasks.index', compact('tasks'));
+    }
+
+    // Show the task creation form
+    public function create() {
+        return view('tasks.create');
+    }
+
+    // Handle form submission and save new task
+    public function store(Request $request) {
+        $request->validate(['title' => 'required']);
+        return redirect('/tasks');
+    }
+
+    // Delete a task
+    public function destroy(Task $task) {
+        $task->delete();
+        return back();
+    }
+}
