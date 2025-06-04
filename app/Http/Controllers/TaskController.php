a51badf1817854688bcfc8771fefee988879afe6
Ruzbihan <ruzbihanmzm@gmail.com>
2025-06-04 16:13:23 +0530

    Controller function and routed edited

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,24 +8,34 @@ use Illuminate\Http\Request;
 class TaskController extends Controller
 {
     //show all tasks
-    public function index() {
+    public function index()
+    {
         $tasks = Task::all();
         return view('tasks.index', compact('tasks'));
     }
 
     // Show the task creation form
-    public function create() {
+    public function create()
+    {
         return view('tasks.create');
     }
 
     // Handle form submission and save new task
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $request->validate(['title' => 'required']);
+
+        Task::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'is_completed' => false
+        ]);
         return redirect('/tasks');
     }
 
     // Delete a task
-    public function destroy(Task $task) {
+    public function destroy(Task $task)
+    {
         $task->delete();
         return back();
     }
